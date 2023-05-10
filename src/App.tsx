69669c235bd32bf32c39f58cfa3e19be7cce082e
Nikhil <nikhilkumarsingh7174@gmail.com>
2023-05-10 23:45:09 +0530

    created UI for the grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  StatusBar,
+  FlatList,
+  Pressable,
+} from 'react-native';
 import React, {useState} from 'react';
 
 import Snackbar from 'react-native-snackbar';
@@ -12,7 +20,7 @@ export default function App() {
   const reloadGame = () => {
     setIsCross(false);
     setGameWinner('');
-    setGameState(new Array(9).fill('empty,0,9'));
+    setGameState(new Array(9).fill('empty', 0, 9));
   };
 
   const checkIsWinner = () => {
@@ -83,7 +91,7 @@ export default function App() {
       setIsCross(!isCross);
     } else {
       return Snackbar.show({
-        text: 'Position is already',
+        text: 'Position is already marked',
         backgroundColor: 'red',
         textColor: '#ffffff',
       });
@@ -93,10 +101,118 @@ export default function App() {
   };
 
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar />
+      {gameWinner ? (
+        <View style={[styles.playerInfo, styles.winnerInfo]}>
+          <Text style={styles.winnerTxt}>{gameWinner}</Text>
+        </View>
+      ) : (
+        <View
+          style={[
+            styles.playerInfo,
+            isCross ? styles.playerX : styles.playerO,
+          ]}>
+          <Text style={styles.gameTurnTxt}>
+            Player {isCross ? 'X' : 'O'}'s Turn
+          </Text>
+        </View>
+      )}
+      {/* Game Grid */}
+      <FlatList
+        numColumns={3}
+        data={gameState}
+        style={styles.grid}
+        renderItem={({item, index}) => (
+          <Pressable
+            key={index}
+            style={styles.card}
+            onPress={() => onChangeItem(index)}>
+            <Icons name={item} />
+          </Pressable>
+        )}
+      />
+      <Pressable style={styles.gameBtn} onPress={reloadGame}>
+        <Text style={styles.gameBtnText}>
+          {gameWinner ? 'Start new game' : 'reload the game'}
+        </Text>
+      </Pressable>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#4D4D4D',
+    height: '100%',
+  },
+  playerInfo: {
+    height: 56,
+
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    borderRadius: 4,
+    paddingVertical: 8,
+    marginVertical: 12,
+    marginHorizontal: 14,
+
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.2,
+    shadowRadius: 1.5,
+  },
+  gameTurnTxt: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    fontWeight: '600',
+  },
+  playerX: {
+    backgroundColor: '#38CC77',
+  },
+  playerO: {
+    backgroundColor: '#F7CD2E',
+  },
+  grid: {
+    margin: 12,
+  },
+  card: {
+    height: 100,
+    width: '33.33%',
+
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    borderWidth: 1,
+    borderColor: '#333',
+  },
+  winnerInfo: {
+    borderRadius: 8,
+    backgroundColor: '#38CC77',
+
+    shadowOpacity: 0.1,
+  },
+  winnerTxt: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    fontWeight: '600',
+    textTransform: 'capitalize',
+  },
+  gameBtn: {
+    alignItems: 'center',
+    marginBottom: 330,
+    padding: 10,
+    borderRadius: 8,
+    marginHorizontal: 36,
+    backgroundColor: '#8D3DAF',
+  },
+  gameBtnText: {
+    fontSize: 18,
+    color: '#FFFFFF',
+    fontWeight: '500',
+  },
+});
